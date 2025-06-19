65c1226df96c1fd4d9b32326394982bd10606dee
H-Janath <janathhma@gmail.com>
2025-06-19 13:32:54 +0530

    fix: update import path for userRolesRoutes and remove commented error handling code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import userRoutes from './users/userRoutes.js';
 import supplierRoutes from './supliers/supliersRoute.js';
-import userRolesRoutes from './users/UserRolesRoutes.js';
+import userRolesRoutes from './roles/UserRolesRoutes.js';
 import customerRoutes from './customers/customerRouter.js';
 import productRoutes from './product/productRoute.js';
 import categoryRoutes from './categories/categoriesRoutes.js';
@@ -22,15 +22,6 @@ app.use(morgan('common'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// Handle bad JSON syntax
-// app.use((err, req, res, next) => {
-//   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-//     return res.status(400).json({ error: 'Invalid JSON format in request body' });
-//   }
-//   next();
-// });
-
-
 
 app.use('/api/users', userRoutes);
 app.use('/api/suppliers', supplierRoutes);
